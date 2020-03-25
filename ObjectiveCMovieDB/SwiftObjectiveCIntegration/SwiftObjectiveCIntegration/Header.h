f47e10f1f1fcd601480efb1ae91eac1077616118
Rafael Ferreira <rafaelfca.ferreira@gmail.com>
2020-03-25 17:35:59 -0300

    created a separate swift aplication to learn how to consume objective c services

---
+++ ObjectiveCMovieDB/SwiftObjectiveCIntegration/SwiftObjectiveCIntegration/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  SwiftObjectiveCIntegration
+//
+//  Created by Rafael Ferreira on 25/03/20.
+//  Copyright © 2020 Rafael Ferreira. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import "MovieDBAPI.h"
+
+#endif /* Header_h */
